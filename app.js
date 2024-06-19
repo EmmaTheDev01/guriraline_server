250fc4649d5c11fe77fdfc2001c1ebe98449b5a6
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-19 12:22:42 +0300

    Initial commit

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
