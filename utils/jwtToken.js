ff3d4d51c82fa6b43b1bfa66f379353aef2dac26
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-20 20:25:58 +0300

    Initial commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "none",
-    secure: req.secure,
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
