fbe76297817f2a2c267835d9c2b71855f6895d98
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-20 13:09:06 +0300

    Revoking changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,18 +1,15 @@
-const sendToken = (user, statusCode, res, req) => {
+// create token and saving that in cookies
+const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
-
-  // Determine if request is secure (HTTPS)
-  const isSecure = req.secure || req.headers['x-forwarded-proto'] === 'https';
 
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none", // Adjust based on your security needs
-    secure: isSecure, // Conditionally set secure based on HTTPS
+    sameSite: "none",
+    secure: true,
   };
 
-  // Set the cookie
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     user,
