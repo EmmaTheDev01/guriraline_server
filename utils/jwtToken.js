d1871fafb6dfed8cc7051223ad62612e74262640
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-20 13:35:58 +0300

    Updating changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,12 +2,11 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "none",
-    secure: true,
+    secure: process.env.NODE_ENV === 'production', // Adjust based on environment
   };
 
   res.status(statusCode).cookie("token", token, options).json({
