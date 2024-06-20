2330d14003b2dc8f08f45e6bdd24c34f02176985
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-06-20 12:54:47 +0300

    Changing API access

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,13 +1,12 @@
-// create token and saving that in cookies
-const sendToken = (user, statusCode, res) => {
+const sendToken = (user, statusCode, res, req) => {
   const token = user.getJwtToken();
 
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none",
-    secure: true,
+    sameSite: "strict", // Adjust based on your security needs
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // Conditionally set secure based on HTTPS
   };
 
   res.status(statusCode).cookie("token", token, options).json({
